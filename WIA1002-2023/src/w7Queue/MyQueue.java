9acc6352742eacd774190d635471843e93bda2fb
jad <jad@10.167.59.246>
2023-05-10 08:21:38 +0800

    Myqueueu

---
+++ WIA1002-2023/src/w7Queue/MyQueue.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package w7Queue;
+import w4LinkedList.Labw4LinkedList.MyLinkedList;
+/**
+ a) Write a generic queue class called MyQueue using LinkedList. Implement the following
+methods:
+a. public MyQueue(E[] e)
+b. public MyQueue()
+c. public void enqueue(E e)
+d. public E dequeue()
+e. public E getElement(int i)
+f. public E peek()
+g. public int getSize()
+h. public boolean contains(E e)
+i. public boolean isEmpty();
+j. public String tostring()
+ * @author jad
+ */
+public class MyQueue {
+    
+}
